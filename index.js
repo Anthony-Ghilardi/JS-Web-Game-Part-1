272fc16ad782bbd15aecd32636fd7c9bd6dd1660
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-03-16 09:32:21 -0700

    WIP

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let greenCharacter = document.createElement('img')
+function newImage (greenCharacter, pineTree) {
+greenCharacter, pineTree = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
+pineTree.src = 'assets/pine-tree.png'
 greenCharacter.style.position = 'fixed'
+pineTree.style.position = 'fixed'
 greenCharacter.style.left = '100px'
+pineTree.style.left = '450px'
 greenCharacter.style.bottom = '100px'
+pineTree.style.bottom = '200px'
 document.body.append(greenCharacter)
+document.body.append(pineTree)
+}
+
+newImage();
