8c28439cd6cc1aab4ad73ca7c189234509eaf2ee
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-03-16 19:12:42 -0700

    DONE

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-function newImage (greenCharacter, pineTree) {
-greenCharacter, pineTree = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-pineTree.src = 'assets/pine-tree.png'
-greenCharacter.style.position = 'fixed'
-pineTree.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-pineTree.style.left = '450px'
-greenCharacter.style.bottom = '100px'
-pineTree.style.bottom = '200px'
-document.body.append(greenCharacter)
-document.body.append(pineTree)
+function newImage (url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
 }
 
-newImage();
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('dblclick', function () {
+        object.remove()
+    })
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
